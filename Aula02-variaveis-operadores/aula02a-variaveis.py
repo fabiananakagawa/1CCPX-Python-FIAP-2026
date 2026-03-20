8db93d6c4fb88fd24e31a8d546901430127be84d
fabianaynakagawa-md <fabiana.ynakagawa@gmail.com>
2026-03-19 22:28:52 -0300

    aula 2 variaveis e operadores

---
+++ Aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,31 @@
+print('olá mundo!')
+
+print(7+4)
+print('7'+"4") #concatenação de strings
+
+#comentarios de uma linha
+
+'''
+comentários de 
+múltiplas simples
+'''
+
+#variaveis
+nome = "alexandre" #string
+idade = "26" #int
+peso = "70.2" #float
+
+print(nome, idade, peso)
+print(f"oiii, {nome}!!!")
+
+
+
+
+#imputs - simulação de formulários no CMD
+nome = input("Digite o seu nome: ")
+idade = int(input("Digite sau idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
