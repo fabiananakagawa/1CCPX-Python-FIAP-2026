8de2c3d0476d52774577a13ed9e4b1ab4df61b17
fabianaynakagawa-md <fabiana.ynakagawa@gmail.com>
2026-03-19 22:57:02 -0300

    aula 2 variaveis e operadores

---
+++ Aula02-variaveis-operadores/exercicio_aula.py
@@ -0,0 +1,12 @@
+
+#desafio 01
+nome = (input("Dígite seu nome: "))
+print(input(f'bem vindo,{nome}!'))
+
+#desafio 02
+dia = (input("Digite o dia do seu aniversário: "))
+mes = (input("Digite o mês do seu aniversário: "))
+ano = (input("Digite o ano do seu aniversário: "))
+print(input(f"Olá,{nome}! Você faz aniversário em {dia, mes, ano}!!"))
+
+
