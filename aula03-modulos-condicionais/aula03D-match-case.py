e66b876f9258a48847cdf6d1cf13a6b9085664e4
fabiananakagawa <fabiana.ynakagawa@gmail.com>
2026-04-09 22:40:12 -0300

    aula 3

---
+++ aula03-modulos-condicionais/aula03D-match-case.py
@@ -0,0 +1,17 @@
+# imagina um programa que recebe a escolha do usuario
+# 0 --> sair do programa
+# 1 --> entrar no programa
+# ---> erro!
+
+escolha_usuario = 1232131
+
+match escolha_usuario:
+    case 0:
+        print("Sair do programa")
+    case 1:
+        print("Entrar no programa")
+    case _:
+        print("Erro!!")
+
+# exercicio 1 = importar biblioteca mp3
+
